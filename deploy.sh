c0f1afed5631ccbcfd7ecebfecd89120cca7b810
vit0r <vitornascimentoaraujo@gmail.com>
2024-10-30 20:32:21 -0300

    add workflow

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NOMINATIM_TAG=4.5.0
-docker build -t vit0r/nominatim:${NOMINATIM_TAG} --target nominatim .
-docker build -t vit0r/nominatim-init:${NOMINATIM_TAG} --target nominatim-init .
-docker build -t vit0r/postgres:${NOMINATIM_TAG} --target postgres .
+docker build -t vit0r/nominatim:${NOMINATIM_TAG} --target nominatim ./image
+docker build -t vit0r/nominatim-init:${NOMINATIM_TAG} --target nominatim-init ./image
+docker build -t vit0r/postgres:${NOMINATIM_TAG} --target postgres ./image
 docker push vit0r/nominatim:${NOMINATIM_TAG}
 docker push vit0r/nominatim-init:${NOMINATIM_TAG}
 docker push vit0r/postgres:${NOMINATIM_TAG}
---
+++ image/nominatim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+until [ "$(pg_isready -h 127.0.0.1)" = "127.0.0.1:5432 - accepting connections" ]
+do 
+    echo "postgres is not ready yet"
+    sleep 5
+done
+if [ ! -f "/data/import.log" ]; then
+    echo "iniciar import"
+    nominatim import --osm-file /data/latest.osm.pbf --verbose
+    nominatim admin --check-database
+    echo "import ok $(date)" > /data/import.log
+fi
+echo > /data/api-access.log
+echo > /data/api-error.log
+gunicorn --bind 0.0.0.0:8000 --access-logfile /data/api-access.log --error-logfile /data/api-error.log -b unix:/data/nominatim.sock -k uvicorn.workers.UvicornWorker nominatim_api.server.falcon.server:run_wsgi
+tail -f /data/api-access.log
