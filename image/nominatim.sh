6288685e2527702034bfe7573fce19982d2884c2
vit0r <vitornascimentoaraujo@gmail.com>
2025-01-18 20:04:22 -0300

    update serve

--- image/nominatim.sh
+++ image/nominatim.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -e
+
 until [ "$(pg_isready -h 127.0.0.1)" = "127.0.0.1:5432 - accepting connections" ]
 do 
     echo "postgres is not ready yet"
-    sleep 5
+    sleep 2
 done
 
 if [ ! -f "/data/import.log" ]; then
     dropdb -h localhost nominatim -f
     echo "import osm file"
-    nominatim import --osm-file /data/latest.osm.pbf --all
+    nominatim import --osm-file /data/latest.osm.pbf --verbose
     echo "$(date)" >> /data/import.log
 fi
 
@@ -19,4 +21,4 @@ gunicorn --bind 0.0.0.0:8000 \
     --capture-output \
     --log-level=debug \
     -b unix:/data/nominatim.sock \
-    -w 4 -k uvicorn.workers.UvicornWorker "nominatim_api.server.falcon.server:run_wsgi()"
+    -k uvicorn.workers.UvicornWorker "nominatim_api.server.falcon.server:run_wsgi()"
