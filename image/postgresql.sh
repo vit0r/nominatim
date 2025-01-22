a797440be4b4732c8a2ec70cecd0aeef89021236
vit0r <vitornascimentoaraujo@gmail.com>
2025-01-22 11:24:20 -0300

    update postgres pghba

--- image/postgresql.sh
+++ image/postgresql.sh
@@ -3,7 +3,7 @@
 if [ ! -d $PGDATA ]; then
     initdb -D $PGDATA
     echo "local all all trust" > $PGDATA/pg_hba.conf
-    echo "host all all trust" >> $PGDATA/pg_hba.conf
+    echo "host all all 0.0.0.0/0 md5" >> $PGDATA/pg_hba.conf
     echo "maintenance_work_mem = 10GB" >> $PGDATA/postgresql.conf
     echo "autovacuum_work_mem = 2GB" $PGDATA/postgresql.conf
     echo "work_mem = 50MB" >> $PGDATA/postgresql.conf
@@ -21,6 +21,7 @@ if [ ! -d $PGDATA ]; then
     echo "full_page_writes = off" >> $PGDATA/postgresql.conf
     sed -i 's/shared_buffers = 128MB/shared_buffers = 2GB/' $PGDATA/postgresql.conf
     sed -i 's/max_wal_size = 1GB/max_wal_size = 4GB/' $PGDATA/postgresql.conf
+    sed -i 's/#listen_addresses = 'localhost'/listen_addresses = '\*'/' $PGDATA/postgresql.conf
 fi
 mkdir -p $PGHOST
 pg_ctl -w -m immediate -l $PGDATA/postgres.log -o "-k $PGHOST -c config_file=$PGDATA/postgresql.conf" start
