cca7b206df706a5c6c903c7d76d0004f28e2db9d
vit0r <vitornascimentoaraujo@gmail.com>
2025-05-30 13:10:32 -0300

    update checck db exists

--- image/nominatim.sh
+++ image/nominatim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
 until [ "$(pg_isready -h $NOMINATIM_DATASE_HOST)" = "$NOMINATIM_DATASE_HOST:5432 - accepting connections" ]
 do 
     echo "postgres $NOMINATIM_DATASE_HOST is not ready yet"
     sleep 2
 done
-if [ ! -f "/data/import.log" ]; then
+DB_EXISTS=$(psql -h $NOMINATIM_DATASE_HOST -c "SELECT 1 as exists FROM pg_database WHERE datname='$POSTGRES_DB';")
+if [ ! -f "/data/import.log" ] && [ $DB_EXISTS=="exists -------- 0" ]; then
     echo "import osm file"
     nominatim import --osm-file /data/latest.osm.pbf --verbose
     echo "$(date)" >> /data/import.log
